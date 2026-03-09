831b0cf67c758b37feb76264dc5c18e1a92b07b9
Sarafat2002 <sarafathossen58@gmail.com>
2026-03-09 17:39:48 +0600

    btn toggling and lodding spin done

--- card.js
+++ card.js
@@ -7,20 +7,89 @@ const allData = () => {
         .then(data => {
             globalIsuuesData = data.data;
             displayData(globalIsuuesData);
-        } );
+            updateIssueCount(globalIsuuesData);
+            hideLoading();
+        });
 }
+
+//loding
+const showLoading = () => {
+    document.getElementById("loading").classList.remove("hidden");
+}
+
+const hideLoading = () => {
+    document.getElementById("loading").classList.add("hidden");
+}
+
+//btn active color
+const buttons = document.querySelectorAll("#filter-buttons button");
+
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // Remove active color from all buttons
+        buttons.forEach(b => b.classList.remove("bg-blue-500", "text-white"));
+        buttons.forEach(b => b.classList.add("bg-gray-300"));
+
+        // Add active color to clicked button
+        btn.classList.add("bg-blue-500", "text-white");
+        btn.classList.remove("bg-gray-300");
+
+        // Get status from data attribute
+        const status = btn.getAttribute("data-status");
+        filterIssues(status);
+    });
+});
+
+
 
 //btn 3 tar jonno
 
+const filterIssues = (status) => {
+    showLoading(); 
+
+
+    setTimeout(() => {
+        let filteredData;
+
+        if (status === "all") {
+            filteredData = globalIsuuesData;
+        } else {
+            filteredData = globalIsuuesData.filter(
+                item => item.status.toLowerCase() === status.toLowerCase()
+            );
+        }
+
+        displayData(filteredData);
+        updateIssueCount(filteredData);
+
+        hideLoading(); 
+    }, 200); 
+}
+
+// card r length ber karar jonno 
+
+const updateIssueCount = (filteredData) => {
+    document.getElementById("issue-count").textContent = `${filteredData.length} Issues`;
+}
+
+//api the data niye card create
 
 const displayData = (informations) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
     for (let info of informations) {
         const cardDiv = document.createElement('div');
+         let borderColor = "";
+        if (info.status.toLowerCase() === "open") {
+            borderColor = "border-t-4 border-emerald-600"; 
+        } else if (info.status.toLowerCase() === "closed") {
+            borderColor = "border-t-4 border-violet-600";
+        }
+
         cardDiv.innerHTML = `
          
-            <div class="bg-white shadow-sm p-4 rounded-md h-full ">
+            <div class="bg-white shadow-sm p-4 rounded-md h-full ${borderColor} ">
 
                 <div class="flex justify-between py-3">
                     <span class="bg-[#00A96E] rounded-full p-3 w-10 h-10 flex items-center justify-center"><i
